c638fdd35f31ce22793dd902cb847a1ca44f067e
Kaoz <kaoz415@gmail.com>
2022-02-08 00:09:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import {JumbotronContainer} from './containers/jumbotron';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages'
 
-function App() {
+export default function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer/>
-      <FooterContainer/>
-    </>
+    <Router>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home/>} />
+        <Route path={ROUTES.BROWSE} element={<Browse/>} />
+        <Route path={ROUTES.SIGN_IN} element={<Signin/>} />
+        <Route path={ROUTES.SIGN_UP} element={<Signup/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
