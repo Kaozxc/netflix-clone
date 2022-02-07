4fc712c63bc6710d88aba9dd3a5fae9a6dda5ca3
Kaoz <kaoz415@gmail.com>
2022-02-07 15:58:58 +0100

    accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { FaqsContainer } from "./containers/faqs";
+import { FooterContainer } from "./containers/footer";
 import {JumbotronContainer} from './containers/jumbotron';
 
 function App() {
   return (
-    <JumbotronContainer>
-    </JumbotronContainer>
+    <>
+      <JumbotronContainer/>
+      <FaqsContainer/>
+      <FooterContainer/>
+    </>
   );
 }
 
