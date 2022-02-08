ac504e6dbd799dd9451bf2a0fe1002b6002be04d
Kaoz <kaoz415@gmail.com>
2022-02-08 08:51:20 +0100

    debugging

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 import {JumbotronContainer} from '../containers/jumbotron';
 
 export default function Home() {
-    return (<>
-        <JumbotronContainer/>,
-        <FaqsContainer/>
-        <FooterContainer/>
-    </>)
+    return (
+    <>
+        <HeaderContainer>
+            <JumbotronContainer/>,
+            <FaqsContainer/>
+            <FooterContainer/>
+        </HeaderContainer>
+    </>
+    );
 }
