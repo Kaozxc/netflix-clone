7c61a9470cfd2cbe1f19b7cb590e1ccf60335a67
Kaoz <kaoz415@gmail.com>
2022-02-10 11:12:53 +0100

    styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link as ReactRouterLink} from 'react-router-dom';
 
-import { Background, ButtonLink, Container, Logo, Feature, Text, FeatureCallOut,  } from './styles/header';
+import { Background, ButtonLink, Container, Link, Logo, Feature,
+     Text, FeatureCallOut, Group, Picture, Profile, Dropdown, Search,
+      SearchIcon, SearchInput, PlayButton } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -15,12 +17,49 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 };
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps}) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`}/>
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+    const [searchActive, setSearchActive] = useState(false);
+
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(searchActive => !searchActive)}>
+                <img src="/images/icons/search.png" alt="Search"/>
+            </SearchIcon>
+            <SearchInput value={searchTerm} onChange={({ target }) => setSearchTerm(target.value)} placeholder="Search films and series" active={searchActive} />
+        </Search>
+    )
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps}) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps}) {
+    return <Profile {...restProps}>{children}</Profile>
+}
+
 Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}) {
     return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
 }
 
 Header.Text = function HeaderText({children, ...restProps}) {
     return <Text {...restProps}>{children}</Text>
+}
+
+Header.Dropdown = function HeaderDropdown({children, ...restProps}) {
+    return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.TextLink = function HeaderTextLink({children, ...restProps}) {
+    return <Link {...restProps}>{children}</Link>
 }
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
