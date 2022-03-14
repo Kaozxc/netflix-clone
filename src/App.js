68d81cad86cd53a1d85122bf757accc9ae3b3d38
Kaoz <kaoz415@gmail.com>
2022-03-14 11:06:44 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages'
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 import { useAuthListener } from './hooks';
 
 export default function App() {
-  const { user } = useAuthListener();
-  //const user = null;
-  
+  //const { user } = useAuthListener();
+  const user = null;
+
   return (
     <Router>
       <Routes>
-       <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} element={<Signin/>}>
+        <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} element={<Signin />}>
         </Route>
-        <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} element={<Signup/>}>
-        </Route> 
-       <Route user={user} path={ROUTES.BROWSE} element={<Browse/>}>
-        </Route>  
-        <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} element={<Home/>}>
+        <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} element={<Signup />}>
+        </Route>
+        <Route user={user} path={ROUTES.BROWSE} element={<Browse />}>
+        </Route>
+        <Route user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} element={<Home />}>
         </Route>
       </Routes>
     </Router>
